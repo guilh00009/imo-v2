c2db4ef3ebc595bd67ec6a6c853e30d618cc59c1
GUILHERME KELLER <121647297+guilh00009@users.noreply.github.com>
2024-12-17 17:19:44 -0300

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,9 +3,14 @@ import tailwindConfig from './tailwind.config.ts';
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: { config: tailwindConfig },
+    tailwindcss: { 
+      config: tailwindConfig 
+    },
     autoprefixer: {},
   },
 };
+
+// Explicitly prevent fallback to JavaScript config
+config.plugins.tailwindcss.config = tailwindConfig;
 
 export default config;
