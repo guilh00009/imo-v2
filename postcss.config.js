f00bd9ecc1306818d6e1b87fe6a84d7430080524
GUILHERME KELLER <121647297+guilh00009@users.noreply.github.com>
2024-12-17 17:57:51 -0300

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-// This file is intentionally left empty to prevent JavaScript config from being used
-// All Tailwind and PostCSS configuration is now in postcss.config.mjs
 module.exports = {
-  plugins: {}
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
