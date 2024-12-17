2ade150f64ae35371b51ed2c6c9a59cca63b94c1
GUILHERME KELLER <121647297+guilh00009@users.noreply.github.com>
2024-12-17 17:19:09 -0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,5 @@
+// This file is intentionally left empty to prevent JavaScript config from being used
+// All Tailwind and PostCSS configuration is now in postcss.config.mjs
+module.exports = {
+  plugins: {}
+};
